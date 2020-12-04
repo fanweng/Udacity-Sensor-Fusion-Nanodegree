80be98228972e525dea72ef4f360045988db2361
Fan Weng <fanweng02@gmail.com>
2020-12-03 21:22:19 -0500

    Render the point cloud.

--- Lidar_Obstacle_Detection/src/environment.cpp
+++ Lidar_Obstacle_Detection/src/environment.cpp
@@ -42,13 +42,14 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // ----------------------------------------------------
 
     // RENDER OPTIONS
-    bool renderScene = true;
+    bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
 
     // Create lidar sensor
     Lidar* lidar = new Lidar(cars, 0);
     pcl::PointCloud<pcl::PointXYZ>::Ptr pointCloud = lidar->scan();
-    renderRays(viewer, lidar->position, pointCloud);
+    // renderRays(viewer, lidar->position, pointCloud);
+    renderPointCloud(viewer, pointCloud, "pointCloud");
 
     // TODO:: Create point processor
 
