f19f751d5316c90f054f8d2bb11650db9ac7a5b3
Fan Weng <fanweng02@gmail.com>
2020-12-22 21:26:04 -0800

    CD-31 #comment Implement search a point in the KD-Tree #time 30m.

--- Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
+++ Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
@@ -27,8 +27,7 @@ struct KdTree
 
     void insertHelper(Node *&node, uint depth, std::vector<float> point, int id)
     {
-        if (node == NULL) // if traversing arrives a NULL node
-        {
+        if (node == NULL) { // if traversing arrives a NULL node
             node = new Node(point, id);
         }
         else { // traversing the node
@@ -43,10 +42,51 @@ struct KdTree
         insertHelper(root, 0, point, id);
     }
 
+    // check if the node point is within a boxed square that is 2x distanceTol for length, centered around the target point
+    bool isInBox(std::vector<float> target, std::vector<float> point, float distanceTol)
+    {
+        if ((point[0] < (target[0] - distanceTol)) || (point[0] > (target[0] + distanceTol)) || (point[1] < (target[1] - distanceTol)) || (point[1] > (target[1] + distanceTol))) {
+            return false;
+        }
+        return true;
+    }
+
+    // calculate if the distance between node point and target point is within tolerance
+    bool isInDistanceTol(std::vector<float> target, std::vector<float> point, float distanceTol)
+    {
+        float d = sqrtf(pow((target[0] - point[0]), 2.0) + pow((target[1] - point[1]), 2.0));
+
+        if (d < distanceTol) {
+            return true;
+        }
+        return false;
+    }
+
+    void searchHelper(std::vector<float> target, Node* node, uint depth, float distanceTol, std::vector<int>& ids)
+    {
+        if (node != NULL) {
+            if (isInBox(target, node->point, distanceTol)) {
+                if (isInDistanceTol(target, node->point, distanceTol)) {
+                    // add point within distanceTol to the return list
+                    ids.push_back(node->id);
+                }
+            }
+
+            // branch to the next node depending on if the boxed square crosses over the divided x or y region
+            if ((target[depth % 2] - distanceTol) < node->point[depth % 2]) {
+                searchHelper(target, node->left, depth+1, distanceTol, ids);
+            }
+            if ((target[depth % 2] + distanceTol) >= node->point[depth % 2]) {
+                searchHelper(target, node->right, depth+1, distanceTol, ids);
+            }
+        }
+    }
+
     // return a list of point ids in the tree that are within distance of target
     std::vector<int> search(std::vector<float> target, float distanceTol)
     {
         std::vector<int> ids;
+        searchHelper(target, root, 0, distanceTol, ids);
         return ids;
     }
 };
