85cc8c67cd2f583271c9ee34505688f0ca5cb1ac
Fan Weng <fanweng02@gmail.com>
2020-12-27 23:52:37 -0500

    CD-44 #comment Apply segmentation and clustering to filtered point cloud #time 1h.

--- Lidar_Obstacle_Detection/src/environment.cpp
+++ Lidar_Obstacle_Detection/src/environment.cpp
@@ -90,7 +90,29 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer)
 
     // TODO: use leaf size of 0.2m, experiment with the other hyperparameters
     pcl::PointCloud<pcl::PointXYZI>::Ptr filteredCloud = pointProcessorI->FilterCloud(inputCloud, 0.2, Eigen::Vector4f(-30, -6.5, -3, 1), Eigen::Vector4f(30, 6.5, 10, 1));
-    renderPointCloud(viewer, filteredCloud, "filteredCloud");
+    // renderPointCloud(viewer, filteredCloud, "filteredCloud");
+
+    // Segment the filtered point cloud
+    std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr, pcl::PointCloud<pcl::PointXYZI>::Ptr> segmentCloud = pointProcessorI->SegmentPlane(filteredCloud, 100, 0.2);
+    renderPointCloud(viewer, segmentCloud.first, "obstacleCloud", Color(1,0,0));
+    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0,1,0));
+
+    // TODO: Cluster the obstacles, experiment with clustering size
+    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(segmentCloud.first, 0.5, 10, 585);
+    int clusterId = 0;
+    std::vector<Color> colors = {Color(1,0,0), Color(0,1,1), Color(1,1,0)};
+
+    for (pcl::PointCloud<pcl::PointXYZI>::Ptr cluster : cloudClusters)
+    {
+        std::cout << "Cluster " << std::to_string(clusterId) << " has a size of ";
+        pointProcessorI->numPoints(cluster);
+        renderPointCloud(viewer, cluster, "obstcleCloud" + std::to_string(clusterId), colors[clusterId % colors.size()]);
+
+        Box box = pointProcessorI->BoundingBox(cluster);
+        renderBox(viewer, box, clusterId);
+
+        ++clusterId;
+    }
 }
 
 
