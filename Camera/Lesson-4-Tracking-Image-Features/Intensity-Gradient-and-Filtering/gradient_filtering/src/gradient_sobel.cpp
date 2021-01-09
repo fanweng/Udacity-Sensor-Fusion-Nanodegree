73a5b013c6fc2f664c999581902742bd7aa11e35
Fan Weng <fanweng02@gmail.com>
2021-01-09 17:35:51 -0500

    CD-45 #comment Implement sobel operator to compute gradient #time 40m.

--- Camera/Lesson-4-Tracking-Image-Features/Intensity-Gradient-and-Filtering/gradient_filtering/src/gradient_sobel.cpp
+++ Camera/Lesson-4-Tracking-Image-Features/Intensity-Gradient-and-Filtering/gradient_filtering/src/gradient_sobel.cpp
@@ -8,10 +8,41 @@ using namespace std;
 
 void gradientSobel()
 {
-    // TODO: Based on the image gradients in both x and y, compute an image 
-    // which contains the gradient magnitude according to the equation at the 
-    // beginning of this section for every pixel position. Also, apply different 
-    // levels of Gaussian blurring before applying the Sobel operator and compare the results.
+    // load image from file
+    cv::Mat img;
+    img = cv::imread("../images/img1.png");
+
+    // convert image to grayscale
+    cv::Mat imgGray;
+    cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
+
+    // create filter kernel
+    float sobel_x[9] = {-1, 0, +1,
+                        -2, 0, +2, 
+                        -1, 0, +1};
+    cv::Mat kernel_x = cv::Mat(3, 3, CV_32F, sobel_x);
+
+    // apply filter
+    cv::Mat result_x;
+    cv::filter2D(imgGray, result_x, -1, kernel_x, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
+
+    // create filter kernel for y direction
+    float sobel_y[9] = {-1, -2, -1,
+                         0,  0,  0, 
+                        +1, +2, +1};
+    cv::Mat kernel_y = cv::Mat(3, 3, CV_32F, sobel_y);
+
+    // apply filter
+    cv::Mat result_y;
+    cv::filter2D(imgGray, result_y, -1, kernel_y, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
+
+    // show result
+    string windowName = "Sobel operator (x-direction and y-direction)";
+    cv::namedWindow(windowName, 1); // create window 
+    cv::imshow(windowName, result_x);
+    cv::waitKey(0); // wait for keyboard input before continuing
+    cv::imshow(windowName, result_y);
+    cv::waitKey(0); // wait for keyboard input before continuing
 }
 
 int main()
