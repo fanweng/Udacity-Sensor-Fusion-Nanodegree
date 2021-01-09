6854a85edb63e91aa7677477af159d3963b0d2f7
Fan Weng <fanweng02@gmail.com>
2021-01-09 16:45:58 -0500

    CD-45 #comment Correct the computing of Gaussian kernel coefficients #time 1h.

--- Camera/Lesson-4-Tracking-Image-Features/Intensity-Gradient-and-Filtering/gradient_filtering/src/gaussian_smoothing.cpp
+++ Camera/Lesson-4-Tracking-Image-Features/Intensity-Gradient-and-Filtering/gradient_filtering/src/gaussian_smoothing.cpp
@@ -13,12 +13,19 @@ void gaussianSmoothing1()
     img = cv::imread("../images/img1gray.png");
 
     // create filter kernel
-    float gauss_data[25] = {1, 4, 7, 4, 1,
-                            4, 16, 26, 16, 4,
-                            7, 26, 41, 26, 7,
-                            4, 16, 26, 16, 4,
-                            1, 4, 7, 4, 1};
+    int gauss_size = 25;
+    float gauss_data[gauss_size] = {1, 4, 7, 4, 1,
+                                    4, 16, 26, 16, 4,
+                                    7, 26, 41, 26, 7,
+                                    4, 16, 26, 16, 4,
+                                    1, 4, 7, 4, 1};
     cv::Mat kernel = cv::Mat(5, 5, CV_32F, gauss_data);
+
+    // calculate the normalized kernel coefficients
+    float sum = accumulate(gauss_data, gauss_data + gauss_size, 0.0);
+    for (int i = 0; i < gauss_size; i++) {
+        gauss_data[i] /= sum;
+    }
 
     // apply filter
     cv::Mat result;
