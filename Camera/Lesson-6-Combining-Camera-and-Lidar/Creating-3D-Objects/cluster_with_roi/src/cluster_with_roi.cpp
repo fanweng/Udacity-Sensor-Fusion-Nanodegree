2daafd2eba567226a12e01938b33da5d29e4ca19
Fan Weng <fanweng02@gmail.com>
2021-01-28 22:57:00 -0500

    CD-51 #comment Display information regarding the bounding boxes #time 30m.

--- Camera/Lesson-6-Combining-Camera-and-Lidar/Creating-3D-Objects/cluster_with_roi/src/cluster_with_roi.cpp
+++ Camera/Lesson-6-Combining-Camera-and-Lidar/Creating-3D-Objects/cluster_with_roi/src/cluster_with_roi.cpp
@@ -123,6 +123,74 @@ void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<Li
     } // eof loop over all Lidar points
 }
 
+void show3DObjects(std::vector<BoundingBox>& boundingBoxes, cv::Size worldSize, cv::Size imageSize, bool bWait)
+{
+    // create a top view
+    cv::Mat topviewImg(imageSize, CV_8UC3, cv::Scalar(255, 255, 255));
+
+    for (auto it1 = boundingBoxes.begin(); it1 != boundingBoxes.end(); ++it1)
+    {
+        // create randomized color for current 3D object
+        cv::RNG rng(it1->boxID);
+        cv::Scalar currColor = cv::Scalar(rng.uniform(0, 150), rng.uniform(0, 150), rng.uniform(0, 150));
+
+        // plot Lidar points into topview image
+        int top = 1e8, left = 1e8, bottom = 0.0, right = 0.0;
+        float xwmin = 1e8, ywmin = 1e8, ywmax = -1e8;
+        for (auto it2 = it1->lidarPoints.begin(); it2 != it1->lidarPoints.end(); ++it2)
+        {
+            // world coordinates
+            float xw = (*it2).x;    // world position in m with x facing forward from sensor
+            float yw = (*it2).y;    // world position in m with y facing left from sensor
+            xwmin = xwmin < xw ? xwmin : xw;
+            ywmin = ywmin < yw ? ywmin : yw;
+            ywmax = ywmax < yw ? ywmax : yw;
+
+            // topview coordinates
+            int y = (-xw * imageSize.height / worldSize.height) + imageSize.height;
+            int x = (-yw * imageSize.width / worldSize.width) + imageSize.width / 2;
+
+            // find enclosing rectangle
+            top = top < y ? top : y;
+            left = left < x ? left : x;
+            bottom = bottom > y ? bottom : y;
+            right = right > x ? right : x;
+
+            // draw individual point
+            cv::circle(topviewImg, cv::Point(x, y), 4, currColor, -1);
+        }
+
+        // draw enclosing rectangle
+        cv::rectangle(topviewImg, cv::Point(left, top), cv::Point(right, bottom), cv::Scalar(0, 0, 0), 2);
+
+        // augment object with key data
+        char str1[200], str2[200], str3[200];
+        sprintf(str1, "id=%d, #pts=%d", it1->boxID, (int)it1->lidarPoints.size());
+        putText(topviewImg, str1, cv::Point2f(left, bottom+50), cv::FONT_ITALIC, 1, currColor);
+        sprintf(str2, "xmin=%2.2f m", xwmin);
+        putText(topviewImg, str2, cv::Point2f(left, bottom+100), cv::FONT_ITALIC, 1, currColor);
+    }
+
+    // plot distance markers
+    float lineSpacing = 1.0;    // gap between distance markers
+    int nMarkers = floor(worldSize.height / lineSpacing);
+    for (size_t i = 0; i < nMarkers; ++i)
+    {
+        int y = (-(i * lineSpacing) * imageSize.height / worldSize.height) + imageSize.height;
+        cv::line(topviewImg, cv::Point(0, y), cv::Point(imageSize.width, y), cv::Scalar(255, 0, 0));
+    }
+
+    // display image
+    string windowName = "3D Objects";
+    cv::namedWindow(windowName, 1);
+    cv::imshow(windowName, topviewImg);
+
+    if (bWait)
+    {
+        cv::waitKey(0);
+    }
+}
+
 int main()
 {
     std::vector<LidarPoint> lidarPoints;
@@ -132,13 +200,15 @@ int main()
     readBoundingBoxes("../dat/C53A3_currBoundingBoxes.dat", boundingBoxes);
 
     clusterLidarWithROI(boundingBoxes, lidarPoints);
-    for (auto it = boundingBoxes.begin(); it != boundingBoxes.end(); ++it)
-    {
-        if (it->lidarPoints.size() > 0)
-        {
-            showLidarTopview(it->lidarPoints, cv::Size(10.0, 25.0), cv::Size(1000, 2000));
-        }
-    }
+    // for (auto it = boundingBoxes.begin(); it != boundingBoxes.end(); ++it)
+    // {
+    //     if (it->lidarPoints.size() > 0)
+    //     {
+    //         showLidarTopview(it->lidarPoints, cv::Size(10.0, 25.0), cv::Size(1000, 2000));
+    //     }
+    // }
+
+    show3DObjects(boundingBoxes, cv::Size(10.0, 25.0), cv::Size(1000, 2000), true);
 
     return 0;
 }
